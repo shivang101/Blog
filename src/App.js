512e086e0ec933429104ed02f44fdb0e8979a872
shivang <shivangmathur101@gmail.com>
2022-04-03 15:40:11 +0530

    fixed the bug with exact path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         </nav>
 
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/about">
