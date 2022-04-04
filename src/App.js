98ffd1aaa24448191cee7ffac483193f5fb4d0da
shivang <shivangmathur101@gmail.com>
2022-04-04 14:24:32 +0530

    added redirecting for invalid routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
@@ -34,6 +40,9 @@ function App() {
           </Route>
           <Route path="/articles/:id">
             <Article />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import { useEffect } from "react";
 import { useFetch } from "../hooks/useFetch";
 export default function Article() {
   const { id } = useParams();
@@ -7,6 +8,17 @@ export default function Article() {
 
   const url = `http://localhost:3000/articles/${id}`;
   const { data: article, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      //redirect
+      // history.goBack();
+      setTimeout(() => {
+        history.push("/");
+      }, 2000);
+    }
+  }, [error, history]);
 
   return (
     <div>
