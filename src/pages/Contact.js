4ccb219ea7731dd7fd1b36ac8b5e945681b13ae5
Shivang <shivangmathur101@gmail.com>
2022-04-06 00:47:22 +0530

    added query on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Contact() {
+  const queryString = useLocation().search;
+
+  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString);
+
+  console.log(queryParams);
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quis
         dignissimos. Molestiae ipsam magnam fuga necessitatibus accusantium cum
